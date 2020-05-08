fc056661ddf3373ce3633d1c95822e5196652ea6
AhmetUyar06 <64646954+AhmetUyar06@users.noreply.github.com>
2020-05-09 02:27:03 +0300

    Add files via upload

--- ceng_2034_answer.py
+++ ceng_2034_answer.py
@@ -1,5 +1,8 @@
+from threading import Thread
 import os
+from queue import Queue
 import requests
+from time import time
 
 print(os.getpid())
 
@@ -12,15 +15,36 @@ Core=os.cpu_count()
 if (Core-Last5) < 1:
  exit()
 
+sira = Queue()
+
 urls = ["http://api.github.com",
         "http://bilgisayar.mu.edu.tr",
         "http://python.org",
         "http://akrepnalan.com/ceng2034",
         "http://github.com/caesarsalad/wow"]
- 
-for x in urls:
-  check = requests.get(x)
-  if 200 <= check.status_code and check.status_code < 300:  
-    print (x, "is valid")
-  else:
-    print (x, "is not valid")
+
+start = time()
+def checksite (que):
+
+    while True:
+        url = que.get()
+        check = requests.get(url)
+        if 200 <= check.status_code and check.status_code < 300:
+            print(url, "is valid")
+        else:
+         print(url, "is not valid")
+        que.task_done()
+
+if __name__ == "__main__":
+    basla = time() 
+    for i in range(5):
+        t = Thread(target = checksite, args = (sira,))
+        t.daemon = True
+        t.start()
+
+    for url in urls:
+        sira.put(url)
+
+    sira.join()
+
+print ( (time() - basla), "saniye surdu")
